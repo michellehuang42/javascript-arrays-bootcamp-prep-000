66783403a2d5cc686fb1c71aa1953073e8385de0
MichelleHuang Huang <mh3793@stern.nyu.edu>
2018-03-08 16:09:29 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,32 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray (array,element) {
+  array = array.unshift(element)
+
+}
+
+function destructivelyAddElementToBeginningOfArray (array,element) {
+  array = array.unshift(element)
+}
+
+function accessElementInArray(array,index){
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return array.shift()
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  return array.pop()
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0,array.length - 1)
+}
+
+function 
